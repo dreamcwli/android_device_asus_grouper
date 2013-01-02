89a456cab54258e3e67b41ffe0f87088b78ed2f7
Chia-Wei Li <dreamcwli@gmail.com>
2013-01-03 01:26:50 +0800

    Fix overriding of build properties
    Change-Id: I35c73536df0202f1c79bed8110f93c4620344ecf

--- my_grouper.mk
+++ my_grouper.mk
@@ -7,7 +7,7 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
-PRODUCT_BUILD_PROPERTY_OVERRIDES += \
+PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=nakasi \
     PRIVATE_BUILD_DESC="nakasi-user 4.2.1 JOP40D 533553 release-keys" \
     BUILD_FINGERPRINT="google/nakasi/grouper:4.2.1/JOP40D/533553:user/release-keys"
